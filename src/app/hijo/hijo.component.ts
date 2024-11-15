905ac6c622fcd63c95c8a4691425e8a48b6698bf
CarlosHaroInfante <csi23-chante@colegioaltair.net>
2024-11-15 09:32:51 +0100

    V1

---
+++ src/app/hijo/hijo.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  standalone: true,
+  imports: [],
+  templateUrl: './hijo.component.html',
+  styleUrl: './hijo.component.css'
+})
+export class HijoComponent {
+  @Input() datoHijo: string = 'Vacío';
+  @Output() meHanHechoClick = new EventEmitter<string>();
+
+  disparaElEvento(){
+    this.meHanHechoClick.emit('Hola papá');
+  }
+
+}
---
+++ src/app/padre/padre.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HijoComponent } from "../hijo/hijo.component";
+
+@Component({
+  selector: 'app-padre',
+  standalone: true,
+  imports: [HijoComponent],
+  templateUrl: './padre.component.html',
+  styleUrl: './padre.component.css'
+})
+export class PadreComponent {
+  saludo(){
+    alert('Hola Clase');
+  }
+  datoPadre = 'Dato de ejemplo que viene del padre';
+
+  recibidoDelHijo(mensaje: string){
+    alert(mensaje);
+  }
+}
